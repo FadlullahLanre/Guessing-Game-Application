c10566640445753ecd7cd1ec71fb46f717227fae
Fadlulanre <88833464+FadlullahLanre@users.noreply.github.com>
2022-06-11 15:46:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//Guessing game application
+
+var userName = prompt("Please enter your name ", "Tunde Kehinde\n");
+
+if(userName != null){
+console.log("Welcome " + userName + " to the guessing game!!\n");
+}
+
+
+
+function Guess (a = 1, b = 2) {
+   
+var randomNo = Math.floor(Math.random() * b) + a;
+var userGuess = prompt("Guess a number between 1 and 2");
+
+if (userGuess == randomNo){
+  console.log("You've been promoted to stage 1.\n");
+  b = b + 1;
+  var randomNo1 = Math.floor(Math.random() * b) + a;
+  var userGuess1 = prompt("Guess a number between 1 and 3 ");
+  
+  if (userGuess1 == randomNo1){
+    
+    console.log("You've been promoted to stage 2!!!");
+  }else{
+    console.log("Game Over!")
+    
+  }
+  
+}else {
+
+  console.log("Game Over!")
+}
+
+}
+Guess();
