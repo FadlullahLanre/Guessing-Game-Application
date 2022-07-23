e7b971fee7083b0bf69800aafa22a9ed1a918ad2
FadlullahLanre <88833464+FadlullahLanre@users.noreply.github.com>
2022-07-23 01:54:33 +0100

    modified the index.js file

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-//Guessing game application
+// Guessing game application
 
-//prompts user for their username.
+// prompts user for their username.
 var userName = prompt("Please enter your name ", "Tunde Kehinde\n");
 
-//prints the username.
+// prints the username and the game starts.
 if(userName != null){
 console.log(userName +", " + "Welcome"  + " to the guessing game!!\n");
 }
 
 
-//Guess function
+// Guess function
 function Guess (a = 1, b = 2) {
   
-//Guess between 1 and 2.   
+// Guess method to guess a number between 1 and 2.   
+  
 var randomNo = Math.floor(Math.random() * b) + a;
 var userGuess = prompt("Guess a number between 1 and 2");
-  
-//Stage 1.
+// if the user gets it right, the user moves to the stage 1.
 if (userGuess == randomNo){
   console.log("You've been promoted to stage 1.\n");
+  // The  number range gets increased by 1, as the player progresses to the next level
   b = b + 1;
-  //Guess between 1 and 3.
+  
+  // Guess a number between 1 and 3.
+  
   var randomNo1 = Math.floor(Math.random() * b) + a;
   var userGuess1 = prompt("Guess a number between 1 and 3 ");
   
+  // if the user gets the guess right, the user moves to stage 2. 
   if (userGuess1 == randomNo1){
     
     console.log("You've been promoted to stage 2!!!");
     
   }else{
     console.log("Game Over!")
-    
   }
   
 }else {
-
   console.log("Game Over!")
 }
   
 }
+
 Guess();
