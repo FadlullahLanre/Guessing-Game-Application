f911a8b2b06554ae3469700e7313780c6291e37d
Fadlulanre <88833464+FadlullahLanre@users.noreply.github.com>
2022-10-19 21:14:00 +0000

    updated winning message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Guessing game application
 
 //prompts user for their username.
-var name = prompt("Please enter your name ");fad
+var name = prompt("Please enter your name ");
 
 //prints the username.
 if(name != null){
@@ -17,14 +17,14 @@ var userGuess = prompt("Guess a number between 1 and 2");
   
 //Stage 1.
 if (userGuess == randomNo){
-  console.log("You've been promoted to stage 1.\n");
+  console.log("You've earned a point, and moved to stage 1.\n");
   b = b + 1;
   //Guess between 1 and 3.
   var randomNo1 = Math.floor(Math.random() * b) + a;
   var userGuess1 = prompt("Guess a number between 1 and 3 ");
   
   if (userGuess1 == randomNo1){
-    console.log("You've been promoted to stage 2!!!");
+    console.log("You've earned a point, and moved to stage 2!!!");
     
   }else{
     console.log("Game Over!")
